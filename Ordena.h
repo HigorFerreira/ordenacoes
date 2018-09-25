32178d51761b87faeaddfc732638473ee5f7d580
Higor Ferreira <hfashigor@hotmail.com>
2018-09-25 16:07:54 -0300

    Ajustes no merge sort

--- Ordena.h
+++ Ordena.h
@@ -9,12 +9,15 @@ public:
     Ordena();
     static bool insertionSort(Item ** V, int n);
     static bool selectionSort(Item ** V, int n);
+
     static void intercala(Item **A, Item **B, int ini, int meio, int fim);
     static void mergeSort(Item ** V, int n);
     static void mergeSort(Item **A, Item **B, int ini, int fim);
+
 	//static bool shellSort(Item ** V, int n);
     static bool bubbleSort(Item ** V, int n);
     //static void heapSort(Item ** V, int n);
+    //static void quickSort(Item ** V, int n);
 };
 
 #endif // ORDENA_H
--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main() {
     TestaOrdena *obj = new TestaOrdena;
     obj->setTam(100);
     obj->preencheVetor();
     obj->mostraVetor(obj->getVetor());
     cout<<"Ordenando o vetor\n\n";
-    Ordena::bubbleSort(obj->getVetor(), obj->getTam());
+    Ordena::mergeSort(obj->getVetor(), obj->getTam());
     obj->mostraVetor(obj->getVetor());
 }
